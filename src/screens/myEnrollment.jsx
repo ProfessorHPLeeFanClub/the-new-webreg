9aa10181280ab15f6643ddc803c9a7d70f01d2f2
Gabriel Garcia <80603461+ggarcia453@users.noreply.github.com>
2023-02-12 10:54:18 -0800

    Update myEnrollment.jsx

---
+++ src/screens/myEnrollment.jsx
@@ -0,0 +1,9 @@
+const api_peter = 'https://api.peterportal.org/rest/v0/';
+var XMLHttpRequest = require('xhr2');
+const request = new XMLHttpRequest();
+
+request.open("GET", api_peter);
+request.send();
+
+
+//console.log(JSON.stringify(data, null, 2));
