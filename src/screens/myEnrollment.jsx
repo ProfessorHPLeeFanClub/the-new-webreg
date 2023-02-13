9b166a7b2ddc1af35787a8d11551a16b9738f8e0
Gabriel Garcia <80603461+ggarcia453@users.noreply.github.com>
2023-02-12 16:53:16 -0800

    Update myEnrollment.jsx

--- src/screens/myEnrollment.jsx
+++ src/screens/myEnrollment.jsx
@@ -3,9 +3,10 @@
     How to query
     "field" = "serch terms" where space is replaced with % (e.g. 2022 Fall -> 2022%Fall)
     subseqeuntly add more fields with & (see sample below);
-    
+
 */ 
-query = 'term=2023%20Winter&departmnet=COMPSCI'
+const term = 'term=2023%20Winter'
+query = '{term}&sectionCodes=15100'
 
 const API_PETER = "https://api.peterportal.org/rest/v0/schedule/soc?{query}";
 
@@ -14,4 +15,4 @@ async function getData() {
     console.log(await data.json());
 }
 
-getData();
+console.log(getData());
